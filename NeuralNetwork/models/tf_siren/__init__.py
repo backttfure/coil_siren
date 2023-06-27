89dd5759d298f05249183f26c7dd5beb7ea8dd4a
backttfuture <1339054937@qq.com>
2023-06-28 06:36:09 +0800

    add project

---
+++ NeuralNetwork/models/tf_siren/__init__.py
@@ -0,0 +1,19 @@
+# Generic Components
+#from .siren import Sine
+#from .siren import SIRENInitializer, SIRENFirstLayerInitializer
+#from .siren import SinusodialRepresentationDense, ScaledSinusodialRepresentationDense
+#from .siren_mlp import SIRENModel, ScaledSIRENModel
+
+# Hyper net components
+#from hypernet import NeuralProcessHyperNet
+#from encoder import SetEncoder
+
+#import sys
+#sys.path.append("..")
+#import meta
+# Meta components
+#from meta.meta_siren import HyperHeNormalInitializer
+#from meta.meta_siren import MetaSinusodialRepresentationDense
+#from meta.meta_siren_mlp import MetaSIRENModel
+
+__version__ = '0.0.5'
---
+++ NeuralNetwork/models/tf_siren/meta/__init__.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('..')
+import meta_siren
+import meta_siren_mlp
+#from meta_siren import MetaSinusodialRepresentationDense, HyperHeNormalInitializer
+#from meta_siren_mlp import MetaSIRENModel
